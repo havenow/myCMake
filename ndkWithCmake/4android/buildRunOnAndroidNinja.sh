f82479b10bd007f2d3f65422969eadf4d62c724f
havenow <715667426@qq.com>
2023-09-14 10:22:18 +0800

    Create buildRunOnAndroidNinja.sh

---
+++ ndkWithCmake/4android/buildRunOnAndroidNinja.sh
@@ -0,0 +1,19 @@
+export ANDOIRD_NDK=/Users/wangfeng/Documents/NDK
+export BUILD_ABI=arm64-v8a
+
+rm -r $BUILD_ABI
+mkdir $BUILD_ABI
+cd $BUILD_ABI
+
+cmake \
+-DANDROID_ABI=$BUILD_ABI \
+-DANDROID_PLATFORM=android-21 \
+-DCMAKE_BUILD_TYPE=Debug \
+-DANDROID_NDK=$ANDOIRD_NDK \
+-DCMAKE_TOOLCHAIN_FILE=$ANDOIRD_NDK/build/cmake/android.toolchain.cmake \
+-DANDROID_TOOLCHAIN=clang \
+-DCMAKE_GENERATOR="Ninja" \
+..
+
+ninja
+cd ..
